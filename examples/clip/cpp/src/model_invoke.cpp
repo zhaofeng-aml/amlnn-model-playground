b967f814ed8fbef0d5504f608da9d28b7cbc59df
feng.zhao <fengzhao@users.noreply.github.com>
2026-01-05 19:33:46 +0800

    modify clip note
    Signed-off-by: feng.zhao <fengzhao@users.noreply.github.com>

--- examples/clip/cpp/src/model_invoke.cpp
+++ examples/clip/cpp/src/model_invoke.cpp
@@ -1,16 +1,19 @@
-/****************************************************************************
-*
-*    Copyright (c) 2025  by amlogic Corp.  All rights reserved.
-*
-*    The material in this file is confidential and contains trade secrets
-*    of amlogic Corporation. No part of this work may be disclosed,
-*    reproduced, copied, transmitted, or used in any way for any purpose,
-*    without the express written permission of amlogic Corporation.
-*
-***************************************************************************/
-/*-------------------------------------------
-                Includes
--------------------------------------------*/
+/*
+ * Copyright (C) 2024–2025 Amlogic, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
